3e8fc60e692170adb0966ebb2e1f3b183c0b0042
Ajay Karki <115032238+Ajaykarki13@users.noreply.github.com>
2023-01-12 18:37:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,16 @@
 
 function palindrome(str){
 
+	for(const i=0 ; i<=str.length/2; i++)
+		{
+		let start= str.charAt(i);
+	let end = str.charAt(str.length-1-i);
+		
+			if(start!=end)
+			{
+				return false;
+			}
+		}
+	return true;	
 }
 module.exports = palindrome
