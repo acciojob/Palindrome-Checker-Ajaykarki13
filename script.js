0f76a8fbcbb65dc3b25b40db03037ca71aab72aa
Ajay Karki <115032238+Ajaykarki13@users.noreply.github.com>
2023-01-12 18:54:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // complete the given function
 
 function palindrome(str){
-
-	for(const i=0 ; i<=str.length/2; i++)
+str=str.toLowerCase();
+	for(let i=0 ; i<=str.length/2; i++)
 		{
 		let start= str.charAt(i);
 	let end = str.charAt(str.length-1-i);
